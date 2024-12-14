19d199b356e41008b6b1df4887f967766a280912
jonathnawill <jonathanwilliam11@outlook.com>
2024-12-14 13:30:19 -0300

    feat - Update SecurityFilter.java

--- src/main/java/com/desafio/pacto/config/SecurityFilter.java
+++ src/main/java/com/desafio/pacto/config/SecurityFilter.java
@@ -2,6 +2,8 @@ package com.desafio.pacto.config;
 
 import com.desafio.pacto.services.CustomUserDetailsServiceImpl;
 import com.desafio.pacto.services.TokenService;
+
+import javax.naming.AuthenticationException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -30,19 +32,39 @@ public class SecurityFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
-		var token = this.recoverToken(request);
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-		if (token != null) {
-
-			var login = tokenService.validateToken(token);
-			UserDetails pessoa = customUserDetailsServiceImpl.loadUserByUsername(login);
-
-			var authentication = new UsernamePasswordAuthenticationToken(pessoa, null, pessoa.getAuthorities());
-
-			SecurityContextHolder.getContext().setAuthentication(authentication);
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+			return;
 		}
-		filterChain.doFilter(request, response);
 
+		try {
+			var token = this.recoverToken(request);
+
+			if (token != null) {
+				var login = tokenService.validateToken(token);
+				if (login == null) {
+					throw new AuthenticationException("Token inválido ou expirado") {};
+				}
+
+				UserDetails pessoa = customUserDetailsServiceImpl.loadUserByUsername(login);
+				if (pessoa == null) {
+					throw new AuthenticationException("Usuário não encontrado para o token fornecido") {};
+				}
+
+				var authentication = new UsernamePasswordAuthenticationToken(pessoa, null, pessoa.getAuthorities());
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+		} catch (AuthenticationException e) {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+			return;
+		}
+
+		filterChain.doFilter(request, response);
 	}
 
 	private String recoverToken(HttpServletRequest request) {
@@ -51,7 +73,5 @@ public class SecurityFilter extends OncePerRequestFilter {
 			return null;
 		}
 		return authHeader.replace("Bearer ", "");
-
 	}
-
 }
